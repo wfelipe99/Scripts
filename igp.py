7e1d899147ac52fd7995281b6ec7852e0a8bc949
wfelipe99 <wevellysantos@gmail.com>
2020-04-18 03:47:36 -0300

    Update igp.py

--- igp.py
+++ igp.py
@@ -4,7 +4,6 @@ import json
 import re
 import csv
 from bs4 import BeautifulSoup
-from collections import OrderedDict
 
 ### CONFIGURAÇÃO ###
 ## LOGIN
@@ -14,13 +13,13 @@ last_part_url = '1587102544909'
 csrf_validity = True
 
 ## OPÇÕES
-#'driver' - piloto
+#'driver' - piloto # não desenvolvido
 #'staff' - funcionário
-search_by = 'staff'
+search_by = 'staff' # Só funciona staff, por enquanto
 
 # Qual foi o último ID procurado?
-last_driver_id_searched = 11024067
-last_staff_id_searched = 11024067
+#last_driver_id_searched = 11024067
+#last_staff_id_searched = 11024067
 
 def to_infinity():
     i = 0
@@ -29,16 +28,16 @@ def to_infinity():
         yield i
 
 
-def cleanhtml(raw_html):
-  cleaner = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
-  cleantext = re.sub(cleaner, '', raw_html)
+# def cleanhtml(raw_html):
+#   cleaner = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+#   cleantext = re.sub(cleaner, '', raw_html)
 
-  cleantext = cleantext.split()
-  cleantext = ' '.join(cleantext)
-  #cleantext  = [word for word in cleantext if word.lower() not in words_blacklist]
-  # cleantext = cleantext.replace(" ", "")
+#   cleantext = cleantext.split()
+#   cleantext = ' '.join(cleantext)
+#   #cleantext  = [word for word in cleantext if word.lower() not in words_blacklist]
+#   # cleantext = cleantext.replace(" ", "")
 
-  return cleantext
+#   return cleantext
 
 class IgpSpider(scrapy.Spider):
     name = "igp"
@@ -61,7 +60,7 @@ class IgpSpider(scrapy.Spider):
         #         if employee_id == 2:
         #             break
 
-        yield scrapy.http.JsonRequest(url=f'https://igpmanager.com/index.php?action=fetch&d={search_by}&id=11024065&csrfName={csrf_name}&csrfToken={csrf_token}&_="{last_part_url}', callback=self.filter_data)
+        yield scrapy.http.JsonRequest(url=f'https://igpmanager.com/index.php?action=fetch&d={search_by}&id=2316444&csrfName={csrf_name}&csrfToken={csrf_token}&_="{last_part_url}', callback=self.filter_data)
             #11024065 CD
             #11024066 TD
             #11024067 DR
@@ -69,76 +68,70 @@ class IgpSpider(scrapy.Spider):
     def filter_data(self, response):
         data = json.loads(response.body)
 
-        ### PRA FAZER:
-        ### Transformar o HTML em Json pra conseguir extrair as informações da Staff
+        is_employee_valid = data['valid']
+        is_csrf_valid = data['csrfValid']
 
-        #is_employee_valid = data['valid']
-        html = data['vars']['skillTable'].split()
-        html_to_json = json.dumps(html)
-        #html_to_json = json.dumps(html)
-        #json_decoded = json.loads(html_to_json)
-        #strength = json_decoded[0]
-        #is_csrf_valid = data[] 
+        if is_csrf_valid:
+            if is_employee_valid:
+                print("Entrou if 1")
+                employee_type = response.url.split('&')[1]
+                employee_id = re.search(r'\d+', response.url.split('&')[2]).group(0)
+                is_cd = response.xpath("//text()[contains(.,'Chief Designer')]").get()
+                is_td = response.xpath("//text()[contains(.,'Technical Director')]").get()
 
-        #print(type(data))
-        print(type(html_to_json)) 
-        print(html_to_json)
-        #print(type(html_to_json)) 
-        #print(html_to_json)
-        #print(is_csrf_valid) 
-      
-        ## LÓGICA AINDA É VÁLIDA, BASTA CONSEGUIR TRANSFORMAR O HTML EM JSON
-        # if is_csrf_valid:
-        #     if is_employee_valid:
-        #         employee_type = response.url.split('&')[1]
-        #         employee_id = response.url.split('&')[2]
-        #         is_cd = response.xpath("//text()[contains(.,'Chief Designer')]").get()
-        #         is_td = response.xpath("//text()[contains(.,'Technical Director')]").get()
+                if employee_type == 'd=staff':
+                    if is_cd:
+                        print("Entrou if 2")
+                        good_strength = ['Acceleration', 'Braking', 'Handling', 'Downforce']
+                        good_weakness = ['Cooling', 'Reliability']
+                        starRating = data['vars']['starRating']
+                        skillTable = data['vars']['skillTable']
 
-        #         if employee_type == 'd=staff':
-        #             if is_cd:
-        #                 attributes = ['Acceleration', 'Braking', 'Handling', 'Downforce']
-        #                 html = data['vars']['skillTable']
-        #                 html_to_json = json.loads(html)
+                        level = BeautifulSoup(starRating, 'lxml').body.find(text = True, recursive = False)
+                        level = int(re.search(r'\d+', level).group(0))
 
-        #                 print(html_to_json)
+                        skillTable = [[cell.text for cell in row("td")]
+                                                for row in BeautifulSoup(skillTable, 'lxml')("tr")]
 
-        #             elif is_td:
-        #                 print('É TD')
+                        strength = [word for line in skillTable[2] for word in line.split()]
+                        strength_attribute = [word for word in strength if word in good_strength]
 
-        # else:
-        #     csrf_validity = False
-            
-        # data = json.loads(response.body)
-        # print(self.i)
-        
-        # if search_by == 'staff':
-        #     with open('staff/descartados.csv', 'w', newline='') as file:
-        #         writer = csv.writer(file)
-        #         writer.writerow(["SN", "Name", "Contribution"])
-        #         writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
-        #         writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
-        #         writer.writerow([3, "Guido van Rossum", "Python Programming"])
-        # print(data)
-        # availabe = response.xpath("//*[contains(text(), 'Buy now')]//text()").get()
-        # if availabe:
-        #     print('Disponível para compra')
-        # else:
-        #     print('N disponível para compra')
-        # is_cd = response.xpath("//text()[contains(.,'Chief Designer')]").get()
-        # is_td = response.xpath("//text()[contains(.,'Technical Director')]").get()
-        # strength= response.xpath("//a[contains(.,'Strength')]").get()
+                        weakness = [word for line in skillTable[3] for word in line.split()]
+                        weakness_attribute = [word for word in weakness if word in good_weakness]
 
-        # print(type(strength))
-        # print(strength)
+                        employee_url = f'https://igpmanager.com/app/d=staff&id={employee_id}&tab=attributes'
 
-        # if is_cd:
-        #     print('É Chief Designer')
-        # elif is_td:
-        #     print('É Technical Director')
-        # else:
-        #     print('É Doctor')
-        
+                        if strength_attribute and weakness_attribute:
+                            print("Chamou record")
+                            if strength_attribute[0] == 'Acceleration':
+                                strength_attribute[0] = 'Aceleração'
 
-        # with open('staff.csv', 'w') as f:
-        #     f.write(response.xpath("//*[contains(text(), 'Talent')]//text()").get())
+                            elif strength_attribute[0] == 'Braking':
+                                strength_attribute[0] = 'Frenagem'
+
+                            elif strength_attribute[0] == 'Handling':
+                                strength_attribute[0] = 'Dirigibilidade'
+
+                            else:
+                                strength_attribute[0] = 'Aerodinâmica'
+
+                            if weakness_attribute[0] == 'Cooling':
+                                weakness_attribute[0] = 'Resfriamento'
+                            
+                            else:
+                                weakness_attribute[0] = 'Confiabilidade'
+                                
+                            record_good_cd(employee_id, 'Designer Chefe', level, strength_attribute[0], weakness_attribute[0], employee_url)
+
+        else:
+            global csrf_validity
+            csrf_validity = False
+            # Gravar em qual ID parou
+
+
+def record_good_cd(employee_id, employee_category, level, strength, weakness, url):
+    with open("staff/good_cd.csv", "w") as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        writer.writerow(['ID', 'CATEGORIA', 'LEVEL', 'ESPECIALIDADE', 'FRAQUEZA', 'URL'])
+        writer.writerow([employee_id, employee_category, level, strength, weakness, url])
