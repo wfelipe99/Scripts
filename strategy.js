de6d4afc64e1563542eb65228ee211e985efe6e9
Wevelly Felipe <wevellyfelipe@pm.me>
2020-05-31 23:33:44 -0300

    Update strategy.js

--- strategy.js
+++ strategy.js
@@ -1,19 +1,19 @@
 const Combinatorics = require('js-combinatorics')
 const fs = require('fs')
 
-const trackLaps = 61
-const pitTime = 17
-const pits = 6
+const trackLaps = 57
+const pitTime = 13
+const pits = 7
 
-const tyres = ['SS', 'S', 'M']
+const tyres = ['SS', 'S']
 const lapsWithGoodTyresSS = 8
-const lapsWithGoodTyresS = 15
-const lapsWithGoodTyresM = 17
+const lapsWithGoodTyresS = 12
+const lapsWithGoodTyresM = 14
 const lapsWithGoodTyresH = 15
-const lapTimeSS = 101.246
-const lapTimeS = 101.948
-const lapTimeM = 102.923
-const lapTimeH = 92.930
+const lapTimeSS = 99
+const lapTimeS = 100
+const lapTimeM = 101
+const lapTimeH = 92
 
 const possibilities = Combinatorics.baseN(tyres, pits + 1)
 
@@ -25,10 +25,11 @@ for (let possibility = 0; possibility < possibilities.length; possibility++) {
 
   const totalLap = (lapsWithGoodTyresSS * occurrenceSS) + (lapsWithGoodTyresS * occurrenceS) + (lapsWithGoodTyresM * occurrenceM) + (lapsWithGoodTyresH * occurrenceH)
 
-  if ((totalLap < trackLaps)) {
+  if ((totalLap < trackLaps - 3) || (totalLap > trackLaps + 3)) {
     continue
   } else {
-    const totalTime = ((lapTimeSS * lapsWithGoodTyresSS) * occurrenceSS) + ((lapTimeS * lapsWithGoodTyresS) * occurrenceS) + ((lapTimeM * lapsWithGoodTyresM) * occurrenceM) + ((lapTimeH * lapsWithGoodTyresH) * occurrenceH) + (pitTime * pits)
+    let totalTime = ((lapTimeSS * lapsWithGoodTyresSS) * occurrenceSS) + ((lapTimeS * lapsWithGoodTyresS) * occurrenceS) + ((lapTimeM * lapsWithGoodTyresM) * occurrenceM) + ((lapTimeH * lapsWithGoodTyresH) * occurrenceH) + (pitTime * pits)
+    totalTime = totalTime.toFixed(3)
     const data = `${possibilities.toArray()[possibility]}, ${totalTime}, ${totalLap}`
 
     fs.writeFile('./strategies/strategy.csv', data + '\n', { flag: 'a' }, (error) => {
