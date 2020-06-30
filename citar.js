0d63930e80180b9010ae1962b99e88caa5613074
Wevelly Felipe <wevellyfelipe@pm.me>
2020-06-30 00:31:22 -0300

    Update citar.js

--- citar.js
+++ citar.js
@@ -1,43 +1,62 @@
+"use strict";
+
 $(window).load(function () {
   var chatbox_script = function chatbox_script() {
     var overrided = Chatbox.prototype.refresh;
-    var overrided2 = Chatbox.prototype.send;
-    var textoUsuario = '';
 
     var _this = this;
 
+    var textoCitado = '';
     $('.user-msg').each(function () {
       $(this).find(".user").append("<button class=\"chatbox-citar\" style=\"float: right;\">Citar</button");
     });
     $('.chatbox-citar').click(function () {
-      textoUsuario = $(this).parent().parent().find('.msg > span').text();
-
-      window.chatbox.send()
-      _this.Chatbox.prototype.send(textoUsuario); // var input = $('#chatbox_footer form #message');
-      // input.val(`"${textoUsuario}" ${input.val()}`);
-
+      textoCitado = $(this).parent().parent().find('.msg > span').text();
+      textoCitado = "Cita\xE7\xE3o de Usu\xE1rio: \"".concat(textoCitado, "\"|").concat($('#message').val());
+      $('#message').val(textoCitado);
+      window.chatbox.send();
     });
 
     Chatbox.prototype.refresh = function (data) {
       overrided.call(this, data);
-      console.log(this);
       $('.user-msg').each(function () {
         $(this).find(".user").append("<button class=\"chatbox-citar\" style=\"float: right;\">Citar</button");
       });
       $('.chatbox-citar').click(function () {
-        textoUsuario = $(this).parent().parent().find('.msg > span').text();
-        textoUsuario = "<span>".concat(textoUsuario, "</span>");
-
-        window.chatbox.send()
-        _this.Chatbox.prototype.send(textoUsuario); // var input = $('#chatbox_footer form #message');
-        // input.val(`"${textoUsuario}" ${input.val()}`);
-
+        textoCitado = $(this).parent().parent().find('.msg > span').text();
+        textoCitado = "Cita\xE7\xE3o de Usu\xE1rio: \"".concat(textoCitado, "\"|").concat($('#message').val());
+        $('#message').val(textoCitado);
+        window.chatbox.send();
+      });
+      $(".msg > span:contains('Citação de')").each(function () {
+        var textoComCitacao = $(this).text().split("|");
+        var corTexto = $(this).css("color");
+        var citacao = textoComCitacao[0];
+        var resposta = textoComCitacao[1];
+        $(this).hide();
+        $(this).parent().append("<div class=\"citacao\" style=\"color: ".concat(corTexto, ";  border-left: 4px solid #AA00FF; border-radius: 5px; background-color: #dea02c; color: white; width: max-content; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px; max-width: 70%;\"><p>Usu\xE1rio</p><p>").concat(citacao, "</p></div>"));
+        $(this).parent().append("<span class=\"resposta\" style=\"color: ".concat(corTexto, "; margin-left: 50px;\">").concat(resposta, "</span>"));
       });
     };
 
     Chatbox.prototype.send = function (params) {
-      console.log("".concat(params, " >> params SEND"));
-      console.log(this);
+      if (!params) {
+        params = $('form[name=\'post\']').serialize();
+      }
+
+      var data = params + '&method=send&archives=' + this.archives;
+      $("#message").val('').focus();
+      var self = this;
+      $.ajax({
+        url: this.actionsUrl,
+        type: 'post',
+        data: data,
+        dataType: 'json',
+        cache: false,
+        success: function success(response) {
+          self.refresh(response);
+        }
+      });
     };
   };
 
