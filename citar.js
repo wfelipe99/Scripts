ffd022dcbac56392dbe84fef95a2d31248f5cbe4
Wevelly Felipe <wevellyfelipe@pm.me>
2020-06-30 02:23:52 -0300

    Update citar.js

--- citar.js
+++ citar.js
@@ -3,9 +3,6 @@
 $(window).load(function () {
   var chatbox_script = function chatbox_script() {
     var overrided = Chatbox.prototype.refresh;
-
-    var _this = this;
-
     var textoCitado = '';
     $('.user-msg').each(function () {
       $(this).find(".user").append("<button class=\"chatbox-citar\" style=\"float: right;\">Citar</button");
@@ -63,9 +60,13 @@ $(window).load(function () {
         var usuarioCitado = textoComCitacao[1];
         var resposta = textoComCitacao[2];
         $(this).hide();
-        $(this).parent().append("<div class=\"citacao\" style=\"color: ".concat(corTexto, ";  border-left: 4px solid #AA00FF; border-radius: 5px; background-color: #dea02c; color: white; width: max-content; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px; max-width: 70%;\"><p>").concat(usuarioCitado, ":</p><p>").concat(citacao, "</p></div>"));
+        $(this).parent().append("<div class=\"citacao\" style=\"color: ".concat(corTexto, ";  border-left: 4px solid #AA00FF; border-radius: 5px; background-color: #dea02c; color: white; width: max-content; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px; max-width: 70%;\"><p style='color: #363534; font-weight: bold'>").concat(usuarioCitado, ":</p><p>").concat(citacao, "</p></div>"));
         $(this).parent().append("<span class=\"resposta\" style=\"color: ".concat(corTexto, "; margin-left: 50px;\">").concat(resposta, "</span>"));
-      });
+      }); // Corrigir o scroll para descer automaticamente
+
+      var intScroll = $('#chatbox').scrollTop(); // Pega o atual scroll da div
+
+      $('#chatbox').scrollTop(intScroll + 1000);
     };
 
     Chatbox.prototype.send = function (params) {
@@ -96,6 +97,8 @@ $(window).load(function () {
       $(this.contentDocument || this.contentWindow.document).find("#chatbox").closest("html").find("head").first().each(function () {
         this.appendChild(s.cloneNode(true));
       });
-    } catch (a) {}
+    } catch (a) {
+      console.log(a);
+    }
   });
 });
